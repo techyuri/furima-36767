136a26f5c66c8de1d7a24d24dc857fb3b1bf2ec5
techyuri <89583984+techyuri@users.noreply.github.com>
2021-10-04 20:13:56 +0900

    モデル　テーブルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,13 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:price,:user,:explain,:category_id,:state_id,:burden_id,:delivery_id,:area_id)
+
+  end
 end
---
+++ db/migrate/20211004094019_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name,           null:false
+      t.integer :price,         null:false 
+      t.references :user,       null:false, foreign_key: true
+      t.text :explain,          null:false
+      t.integer :category_id,   null:false
+      t.integer :status_id,     null:false
+      t.integer :burden_id,     null:false
+      t.integer :delivery_id,   null:false
+      t.integer :area_id, null:false
+      t.timestamps
+    end
+  end
+end
