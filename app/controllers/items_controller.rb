3f5029a61c1fea8f4137c68faad3f3eebb78a57b
techyuri <89583984+techyuri@users.noreply.github.com>
2021-10-05 19:16:06 +0900

    バリデーションの編集と脱字の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path(@item)
     else
-      render :index
+      render :new
     end  
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,35 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
+  has_one :buyer
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explain
+    validates :category_id
+    validates :state_id
+    validates :delivery_id
+    validates :burden_id
+    validates :area_id
+    validates :price, format: { with: /\A\d+\z/ },
+                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :category
   belongs_to :state
   belongs_to :burden
   belongs_to :area
   belongs_to :delivery
-  has_one :buyer
-  has_one_attached :image
 
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :delivery_id
+    validates :area_id
+    validates :burden_id
+  end
 end
