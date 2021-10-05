e4956c3ab8de6a368edf0587945997ac9b9b3961
techyuri <89583984+techyuri@users.noreply.github.com>
2021-10-05 19:38:28 +0900

    単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  {"ito"}
+    explain               {"setsumei"}
+    category_id           {"2"}
+    state_id             {"2"}
+    delivery_id       {"2"}
+    area_id    {"45"}
+    burden_id       {"2"}
+    price                 {"5000"}
+    user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.jpeg')
+    end
   end
 end
