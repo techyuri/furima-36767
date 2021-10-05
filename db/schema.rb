6b66ff423d7f3f0e386f7a8874e2ff0c729eeea3
techyuri <89583984+techyuri@users.noreply.github.com>
2021-10-05 16:58:16 +0900

    モデル・テーブルの追加/編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_112345) do
+ActiveRecord::Schema.define(version: 2021_10_05_055750) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
