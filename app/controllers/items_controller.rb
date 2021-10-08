d50f1e1b22fdd97ffddc9b86cece9f40ab2a9988
techyuri <89583984+techyuri@users.noreply.github.com>
2021-10-08 15:53:24 +0900

    不要な記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items,only: [:index, :new,:create,:show,:edit,:update]
-    resources :buyers, only: [:index, :create]
+    #resources :buyers, only: [:index, :create]
 end
