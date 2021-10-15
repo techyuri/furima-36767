7a0d29cdfe3a6ee53179608d3ee51e2188310c92
techyuri <89583984+techyuri@users.noreply.github.com>
2021-10-15 14:40:56 +0900

    重複エラーメッセージの解消

--- app/models/item_form.rb
+++ app/models/item_form.rb
@@ -6,13 +6,14 @@ class ItemForm
     validates :city
     validates :address_line
     validates :phone_number, format: { with: /\A[0-9]+\z/ }
-    validates :phone_number, length: { maximum: 11, minimum: 10 }
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :user_id
     validates :item_id
     validates :token
   end
+
+  validates :phone_number, length: { maximum: 11, minimum: 10 }
 
   def save
     buyer = Buyer.create(user_id: user_id, item_id: item_id)
